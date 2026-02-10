5107d72d8eaa7a029b2c66184ed8b04888846d20
scottkiersten-hash <scottkiersten@gmail.com>
2026-02-10 20:35:36 +0700

    Update config.tsx

--- src/router/config.tsx
+++ src/router/config.tsx
@@ -1,13 +1,6 @@
 import type { RouteObject } from "react-router-dom";
-import NotFound from "../pages/NotFound";
-import Home from "../pages/home/page";
-import Upload from "../pages/upload/page";
-import About from "../pages/about/page";
-import Privacy from "../pages/privacy/page";
-import Terms from "../pages/terms/page";
-import Verify from "../pages/verify/page";
-import HowItWorks from "../pages/how-it-works/page";
-import FAQ from "../pages/faq/page";
+import Home from "../pages/Home";
+import Auto from "../pages/Auto";
 
 const routes: RouteObject[] = [
   {
@@ -15,36 +8,8 @@ const routes: RouteObject[] = [
     element: <Home />,
   },
   {
-    path: "/upload",
-    element: <Upload />,
-  },
-  {
-    path: "/about",
-    element: <About />,
-  },
-  {
-    path: "/privacy",
-    element: <Privacy />,
-  },
-  {
-    path: "/terms",
-    element: <Terms />,
-  },
-  {
-    path: "/verify",
-    element: <Verify />,
-  },
-  {
-    path: "/how-it-works",
-    element: <HowItWorks />,
-  },
-  {
-    path: "/faq",
-    element: <FAQ />,
-  },
-  {
-    path: "*",
-    element: <NotFound />,
+    path: "/auto",
+    element: <Auto />,
   },
 ];
 

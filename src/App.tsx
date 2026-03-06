3af8a30d1fe3b3d4a5f6a0879f8418ca6f5b286a
scottkiersten-hash <scottkiersten@gmail.com>
2026-03-06 18:22:58 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { AppRoutes } from "./router";
 import { I18nextProvider } from "react-i18next";
 import i18n from "./i18n";
 import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
-import Signup from './pages/Signup';
+
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+
+/* ===========================
+SCROLL TO TOP ON ROUTE CHANGE
+=========================== */
+
 function ScrollToTop() {
   const { pathname } = useLocation();
 
@@ -15,12 +22,31 @@ function ScrollToTop() {
   return null;
 }
 
+/* ===========================
+APP
+=========================== */
+
 function App() {
   return (
     <I18nextProvider i18n={i18n}>
       <BrowserRouter basename={__BASE_PATH__}>
+
         <ScrollToTop />
-        <AppRoutes />
+
+        <Routes>
+
+          {/* Existing Site Routes */}
+          <Route path="/*" element={<AppRoutes />} />
+
+          {/* Auth Pages */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+
+          {/* User Dashboard */}
+          <Route path="/dashboard" element={<Dashboard />} />
+
+        </Routes>
+
       </BrowserRouter>
     </I18nextProvider>
   );

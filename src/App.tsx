8650f565d4197afe7fc35b3b6634ead8d949d923
scottkiersten-hash <scottkiersten@gmail.com>
2026-03-12 19:25:57 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AppRoutes } from "./router";
-
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import Dashboard from "./pages/Dashboard";
-import Success from "./pages/Success";
-import Registry from "./pages/registry/Registry";
 
 function App() {
 return ( <BrowserRouter> <Routes>
-
-```
-    <Route path="/*" element={<AppRoutes />} />
-
-    <Route path="/login" element={<Login />} />
-    <Route path="/signup" element={<Signup />} />
-
-    <Route path="/dashboard" element={<Dashboard />} />
-
-    <Route path="/success" element={<Success />} />
-
-    <Route path="/registry" element={<Registry />} />
-
-  </Routes>
-</BrowserRouter>
-```
-
+<Route path="/" element={<div>ProofDeed Running</div>} /> </Routes> </BrowserRouter>
 );
 }
 

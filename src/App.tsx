32dde09d99754c17eff1f899bfdbf0fa5f9f8ded
scottkiersten-hash <scottkiersten@gmail.com>
2026-03-12 22:15:15 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, useParams } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import QRCode from "react-qr-code";
 

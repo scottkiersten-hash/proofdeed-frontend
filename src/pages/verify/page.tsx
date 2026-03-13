79cf26a9099bd263c41cef91d055bc23e921cb9a
scottkiersten-hash <scottkiersten@gmail.com>
2026-03-13 14:08:05 +0700

    Update page.tsx

--- src/pages/verify/page.tsx
+++ src/pages/verify/page.tsx
@@ -1,7 +1,7 @@
 import { useState, FormEvent, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import Header from '../../components/feature/Header';
-import Footer from '../../components/feature/Footer';
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
 import CertificateQR from "../../components/CertificateQR";
 
 export default function Verify() {

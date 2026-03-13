f8f8e473d98a7794fe6efa93b6cf3d98291c6d9a
scottkiersten-hash <scottkiersten@gmail.com>
2026-03-13 17:04:23 +0700

    Update page.tsx

--- src/pages/government/page.tsx
+++ src/pages/government/page.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
-import GovernmentCertificate from '../../components/feature/GovernmentCertificate';
+import GovernmentCertificate from "../certificate/government/page";
 
 export default function Government() {
   const [showCertificate, setShowCertificate] = useState(false);

ece1ab3d4940f55fde881c9b1738a41267659ef1
scottkiersten-hash <scottkiersten@gmail.com>
2026-03-13 14:15:50 +0700

    Update Signup.tsx

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,6 +1,6 @@
 import { useSearchParams } from 'react-router-dom';
 import Header from "../components/Header";
-import Footer from '../components/feature/Footer';
+import Footer from "../components/Footer";
 
 export default function Signup() {
 

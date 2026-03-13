44d85bb6e41d297dcf7e05ea3116d1a4ca8b66db
scottkiersten-hash <scottkiersten@gmail.com>
2026-03-13 16:42:04 +0700

    Update page.tsx

--- src/pages/technical-process/page.tsx
+++ src/pages/technical-process/page.tsx
@@ -1,6 +1,6 @@
 import { useNavigate } from 'react-router-dom';
-import Header from '../../components/Header';
-import Footer from '../../components/Footer';
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
 
 export default function TechnicalProcess() {
   const navigate = useNavigate();

92d45a46536d4e88d66bc8999c34be9c65f3080c
scottkiersten-hash <scottkiersten@gmail.com>
2026-03-13 16:01:23 +0700

    Update page.tsx

--- src/pages/rfp-framework/page.tsx
+++ src/pages/rfp-framework/page.tsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
-import Header from '../../components/feature/Header';
-import Footer from '../../components/feature/Footer';
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
 
 export default function RfpFramework() {
   return (

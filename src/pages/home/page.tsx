7386b336d0fc58718e5196967ee38c22d8ac51e6
scottkiersten-hash <scottkiersten@gmail.com>
2026-03-13 14:07:24 +0700

    Update page.tsx

--- src/pages/home/page.tsx
+++ src/pages/home/page.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
-import Header from "../../components/feature/Header";
-import Footer from "../../components/feature/Footer";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
 
 import governmentImg from "../../assets/hero/government.jpg";
 import documentImg from "../../assets/hero/document.jpg";
